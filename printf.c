997fcfca793761d3c8c275060b23e700778a2f8f
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-10-17 03:42:56 -0700

    mains

--- printf.c
+++ printf.c
@@ -2,28 +2,33 @@
 
 /**
  * _printf - Produces output according to a format
- * @format: Is a character string. The format string
- * is composed of zero or more directives
+ * @format: Is a character string.
  *
  * Return: The number of characters printed 
  **/
 int _printf(const char *format, ...)
 {
-	int size;
+	int printed = 0;
+
 	va_list args;
 
-	if (format == NULL)
-		return (-1);
-
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
-
 	va_start(args, format);
-	size = handler(format, args);
 
-	_putchar(-1);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			printed = selector(format, args, printed);
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
+	}
 	va_end(args);
-
-	return (size);/*return statement*/
+	return (printed);
 }
